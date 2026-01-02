e52fc46052354915a2b27ba3661ac2d2d8d96295
wzx <wfkmsx@gmail.com>
2026-01-02 13:17:33 +0800

    feat: simplify app to single hello page

--- src/pages/home/index.config.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 首页配置
- */
-export default definePageConfig({
-  navigationBarTitleText: '首页'
-})
--- src/pages/profile/index.config.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 个人中心页配置
- */
-export default definePageConfig({
-  navigationBarTitleText: '我的'
-})
