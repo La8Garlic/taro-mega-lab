a4e784a55de5493c0299b227a95ca3db18a135a0
wzx <wfkmsx@gmail.com>
2026-01-02 14:02:43 +0800

    chore: add TSDoc comments for components and unify exports

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,11 +1,34 @@
 import { View } from '@tarojs/components'
 import './index.scss'
 
+/**
+ * 页面容器组件属性
+ * @description PageContainer 组件的属性接口
+ */
 interface PageContainerProps {
+  /** 子元素内容 */
   children: React.ReactNode
+  /** 自定义类名，会附加到默认类名后 */
   className?: string
 }
 
+/**
+ * 页面容器组件
+ * @description 提供统一的页面布局容器，确保所有页面具有一致的内外边距和布局结构
+ *
+ * @example
+ * ```tsx
+ * import PageContainer from '@/components/PageContainer'
+ *
+ * export default function MyPage() {
+ *   return (
+ *     <PageContainer>
+ *       <Text>页面内容</Text>
+ *     </PageContainer>
+ *   )
+ * }
+ * ```
+ */
 export default function PageContainer({ children, className = '' }: PageContainerProps) {
   return (
     <View className={`page-container ${className}`}>
